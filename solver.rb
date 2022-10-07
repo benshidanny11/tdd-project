e5f94e8f40edc7c4baa2269d028109978800629f
shaqayq <shaqayq.darwazi@gmail.com>
2022-10-07 12:44:09 +0430

    Test solver class methods

--- solver.rb
+++ solver.rb
@@ -1,13 +1,29 @@
 class Solver
+
   def factorial(num)
-  #  Implement logic here
+    raise 'Provided number is less than 0' if num.negative?
+    return 1 if num.zero?
+    fact = 1
+    (1..num).each do |i|
+      fact *= i
+    end
+    fact
   end
 
   def reverse(string)
-  #  Implement logic here
+    string.reverse
   end
 
   def fizzbuzz(number)
-    # Implement logic here
+    if (number % 3).zero? && (number % 5).zero?
+      'fizzbuzz'
+    elsif (number % 3).zero?
+      'fizz'
+    elsif (number % 5).zero?
+      'buzz'
+    else
+      number.to_s
+    end
   end
+
 end
